3c112517287f168a64ff4561240be703af142211
acparkgh <acpark123@gmail.com>
2021-04-16 19:06:04 -0400

    use strict mode

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+console.log(hasDriversLicense);
+if (passTest) { hasDriverLicense = true }
+// if (passTest) { hasDriversLicense = true }
+console.log(hasDriverLicense);
+
+console.log(hasDriversLicense);
+
+
+if (hasDriversLicense) { console.log("I can drive :D") };
+
