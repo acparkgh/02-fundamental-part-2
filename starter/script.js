b4603251231f8a8e72d1bd3612e8f5936ba3e577
acparkgh <acpark123@gmail.com>
2021-04-17 02:00:59 -0400

    function expression vs declaration

--- starter/script.js
+++ starter/script.js
@@ -2,14 +2,51 @@
 
 let hasDriversLicense = false;
 const passTest = true;
+const age = 17;
 
-console.log(hasDriversLicense);
-if (passTest) { hasDriverLicense = true }
+// console.log(hasDriversLicense);
+// if (passTest) { hasDriverLicense = true }
 // if (passTest) { hasDriversLicense = true }
-console.log(hasDriverLicense);
+// console.log(hasDriversLicense);
 
-console.log(hasDriversLicense);
+// console.log(`hasDriversLicense`);
+
+// if (hasDriversLicense) { console.log("I can drive :D") };
 
 
-if (hasDriversLicense) { console.log("I can drive :D") };
+// const cutFruitPieces = function (fruit) {
+//   return (fruit * 4);
+// }
+
+function cutFruitPieces(fruit) {
+  return (fruit * 4);
+}
+
+// function declaration:
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+//     const juice = `Juice with ${applePieces} piece of apple ${orangePieces} piece of orange.`;
+//     return juice  
+//   };
+  
+// function expression:
+const fruitProcessor = function (apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} piece of apple ${orangePieces} piece of orange.`;
+  return juice  
+};
+
+const fruitProcessors = (apples, oranges) => {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} piece of apple ${orangePieces} piece of orange.`;
+  return juice  
+}
+
+console.log(fruitProcessor);
+console.log(fruitProcessors);
+
+console.log(fruitProcessors(2, 5));
 
